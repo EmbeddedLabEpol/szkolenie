7d46b7cce0dfd0881fd4121630d91330568cae4e
Kamil Cieckiewicz <kamil.cieckiewicz@gmail.com>
2016-05-17 10:08:28 +0200

    Paddle - mouse move

--- Game.cpp
+++ Game.cpp
@@ -2,6 +2,7 @@
 #include "Ball.h"
 #include "Block.h"
 #include "Menu.h"
+#include "Paddle.h"
 #include <vector>
 #include <SFML/Graphics.hpp>
 #include <iostream>
@@ -13,37 +14,39 @@ Game::Game()
 Game::~Game()
 {
 }
-void Game::generateBlocksFields(std::vector<Block>& blocks, int cols){
+void Game::generateBlocksFields(std::vector<Block>& blocks, int cols)
+{
 	int blockX = 50;
 	int blockY = 30;
 	int nBlocks = WIDTH/blockX;
 	int startPrint = (WIDTH-(nBlocks*blockX))/2;
-    for(int x = 0; x < nBlocks; ++x)
-      for(int y= 0; y < cols; ++y)
+	for(int x = 0; x < nBlocks; ++x)
+		for(int y= 0; y < cols; ++y)
 			blocks.emplace_back(startPrint+(x*blockX), 5+(y*blockY));
 }
 
 int Game::run()
 {
-	
+
 	//Block block;
 	std::vector<Block> blocks;
-	generateBlocksFields(blocks, 5 , 5);
-	Menu menu(WIDTH,HEIGHT);	
+	generateBlocksFields(blocks, 5);
+	Menu menu(WIDTH,HEIGHT);
+	Paddle p(float(WIDTH/2), float(HEIGHT-50));
 // create the window
 	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Arcanoid - nasza wypas wersja 1.0");
 
 	sf::Clock clock; // starts the clock
 	sf::Time elapsed = clock.getElapsedTime();
-  
+
 
 	while (window.isOpen()) {
 		// check all the window's events that were triggered since the last iteration of the loop
 		sf::Event event;
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::MouseMoved) {
-				std::cout << "new mouse x: " << event.mouseMove.x << std::endl;
-				std::cout << "new mouse y: " << event.mouseMove.y << std::endl;
+				float mouseXPosition = event.mouseMove.x;
+				p.movePaddle(mouseXPosition);
 			}
 
 			// "close requested" event: we close the window
@@ -59,15 +62,16 @@ int Game::run()
 		elapsed = clock.getElapsedTime();
 
 		if(elapsed.asMilliseconds() > 100) {
-		
+
 
 			clock.restart();
 		}
 
 		for(auto& block : blocks) window.draw(block.block);
 		//window.draw(block.block);
-		 
-    
+
+
+		p.draw(window);
 		menu.draw_menu(window);
 		window.display();
 
--- Paddle.h
+++ Paddle.h
@@ -1,10 +1,20 @@
 #ifndef PADDLE_H
 #define PADDLE_H
+#include <SFML/Graphics.hpp>
 
 class Paddle
 {
+private:
+	const float paddleWidth = 80, paddleHeight = 20;
+	float paddlePositionX, paddlePositionY;
+	sf::CircleShape leftCircle;
+	sf::RectangleShape rectangle;
+	sf::CircleShape rightCircle;
+
 public:
-	Paddle();
+	Paddle(float, float);
+	void draw(sf::RenderWindow&);
+	void movePaddle(float);
 	~Paddle();
 
 };
