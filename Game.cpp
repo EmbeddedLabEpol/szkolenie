839d45cfe1976c3d50aab0811adc9e20099177b3
ewa <ewa@example.com>
2016-05-17 09:49:54 +0200

    Dziedziczenie dodane do block

--- Game.cpp
+++ Game.cpp
@@ -17,7 +17,7 @@ void Game::generateBlocksFields(std::vector<Block>& blocks, int rows, int cols){
 
     for(int x = 0; x < rows; ++x)
       for(int y= 0; y < cols; ++y)
-			blocks.emplace_back(x*60+5, y*40+5);
+			blocks.emplace_back(x*60, y*40);
 }
 
 int Game::run()
@@ -33,24 +33,52 @@ int Game::run()
 	sf::Clock clock; // starts the clock
 	sf::Time elapsed = clock.getElapsedTime();
   
+	unsigned int r =50;
+
+	sf::CircleShape circle(r);
+	circle.setFillColor(sf::Color::Blue);
+	circle.setOrigin(50, 50);
+	
+	float xc = 400;
+	float yc = 300;
+	
+	circle.setPosition(xc, yc);
+  
 
 	while (window.isOpen()) {
 		// check all the window's events that were triggered since the last iteration of the loop
 		sf::Event event;
+		
+		
+		
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::MouseMoved) {
 				std::cout << "new mouse x: " << event.mouseMove.x << std::endl;
 				std::cout << "new mouse y: " << event.mouseMove.y << std::endl;
+				xc = event.mouseMove.x;
+				yc = event.mouseMove.y;
+				circle.setPosition(xc, yc);
 			}
 
 			// "close requested" event: we close the window
 			if (event.type == sf::Event::Closed)
 				window.close();
+				
+			if (event.type == sf::Event::MouseButtonPressed)
+			{
+					
+				for (auto& block : blocks)
+				{
+					if (block.is_colision(xc, yc, r))
+					 block.setPosition(801, 601);
+				
+				}
+			}
 		}
 
 
 		// clear the window with black color
-		window.clear(sf::Color::Yellow);
+		window.clear(sf::Color::Black);
 
 
 		elapsed = clock.getElapsedTime();
@@ -61,11 +89,11 @@ int Game::run()
 			clock.restart();
 		}
 
-		for(auto& block : blocks) window.draw(block.block);
+		for(auto& block : blocks) window.draw(block);
 		//window.draw(block.block);
 		 
-    
-		menu.draw_menu(window);
+		window.draw(circle);
+		//menu.draw_menu(window);
 		window.display();
 
 	}
